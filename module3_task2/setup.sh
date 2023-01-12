eb5a9522b4101e5a548e2745f7b0a93706152313
Sebastian <sebaslopez.2002@gmail.com>
2023-01-12 09:55:55 -0500

    syntax fixed

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update && apt-get install -y curl make zip
-npm install -g mardownlint-cli
+npm install -g markdownlint-cli
 
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo_extended.deb
 apt-get install -y ./hugo_extended.deb
